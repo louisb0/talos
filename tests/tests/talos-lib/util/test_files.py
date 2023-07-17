64688aeae783e252388f8d9350cd1937c47d4ec3
gLevaa <louisbaragwanath@outlook.com>
2023-07-17 01:14:06 -0400

    Create basic tests for file system interface

---
+++ tests/tests/talos-lib/util/test_files.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import Mock, patch
+import os.path
+
+import logging
+import json
+
+from talos.util.files import write_to_disk, delete_from_disk
+
+
+class TestFiles(unittest.TestCase):
+    def setUp(self):
+        logging.getLogger("talos.logger").setLevel(logging.CRITICAL)
+
+    def test_files(self):
+        paths = ["f1.json", "f2.json", "f3.json"]
+
+        with self.subTest("test_write"):
+            contents = [{"key": "value1"}, {"key": "value2"}, {"key": "value3"}]
+            stored_paths = write_to_disk(contents, paths)
+
+            # same num of files in mem written to disk
+            self.assertEqual(len(paths), len(stored_paths))
+            for i, path in enumerate(stored_paths):
+                # file name preserved (path added under hood)
+                self.assertIn(paths[i], path)
+
+                # assert content
+                with open(path, "r") as f:
+                    self.assertEqual(json.load(f), contents[i])
+
+        with self.subTest("test_delete"):
+            delete_from_disk(paths)
+
+            for path in stored_paths:
+                self.assertFalse(os.path.exists(path))
